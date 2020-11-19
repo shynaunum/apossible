e3af9d20c97e011b28f4a66b3ff1b39e53008210
shynaunum <47771138+shynaunum@users.noreply.github.com>
2020-11-19 09:25:41 +0900

    Add files via upload

---
+++ js/tabMenu.js
@@ -0,0 +1,27 @@
+(function(){
+  $.fn.menuBar = function(){
+    this.each(function(index){
+      var $menuBar = null,
+          $menuList = null,
+          $menuSelect = null;
+
+      function init(el){
+        $menuBar = $(el);
+        $menuList = $menuBar.find('li');
+      }
+      function event(){
+        $menuList.on('click', function(){
+          if($menuSelect)
+            $(this).removeClass('active');
+            $(this).siblings().removeClass('active');
+          $menuSelect = $(this);
+          $menuSelect.addClass('active');
+          $menuSelect.parent('ul').next().children('div').eq($(this).index()).addClass('active').siblings().removeClass('active');
+        });
+      }
+      init($(this));
+      event();
+    });
+    return this;
+  }
+})(jQuery);
